cf767ce86a31d69dc420a7ff6dd2a413d68b2210
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-05-08 18:38:16 +0200

    Added Order Tests

--- Order/Program.cs
+++ Order/Program.cs
@@ -2,26 +2,29 @@
 
 namespace Order
 {
-    internal class Program
+    public class Program
     {
         /// <summary>
         /// Task: https://acm.timus.ru/problem.aspx?space=1&num=1510
         /// </summary>
         static void Main(string[] args)
         {
-            var numberList = new List<int>()
+            var numbersList = new List<int>()
             {
-                5,3,3,2,2,3,9,5,7,7,7,3,7,7,7,5,5
+                5,3,3,2,2,3,9,5,7,7,7,3,7,7,7,5,5,2,2,2,2
             };
 
-            Debugger.Break();
 
-            var mostCommonOne = numberList.GroupBy(x => x)
-                .OrderByDescending(x => x.Count())
-                .Select(x => x.Key)
-                .First();
+            var mostCommonOne = MostCommonOne(numbersList);
 
             Console.WriteLine(mostCommonOne);
+        }
+
+        public static int MostCommonOne(IEnumerable<int> numbers)
+        {
+            return numbers.GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .Select(x => x.Key).First();
         }
     }
 }
