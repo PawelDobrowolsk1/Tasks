7a313dd15ea834627136501022c08f3296be87c6
PawelDobrowolsk1 <pdobrowolski001@gmail.com>
2023-05-09 01:15:05 +0200

    Added Salary Tests

--- Salary/Program.cs
+++ Salary/Program.cs
@@ -21,41 +21,86 @@ namespace Salary
         {
             var hightToLow = salary[salary.Length - 1] > salary[0];
             int z = salary.Length % 2 == 0 ? 1 : 2;
-
+            bool onlyNine = true;
 
             var salaryAsStringBuilder = new StringBuilder(salary);
 
-            salaryAsStringBuilder[salary.Length - 1] = salary[0];
 
-            for (int i = 1, j = salary.Length - 2; i < j; i++, j--)
+
+            for (var i = 1; i <= salary.Length - 1; i++)
             {
-                var x = Convert.ToInt32(salaryAsStringBuilder[i].ToString());
-                var y = Convert.ToInt32(salaryAsStringBuilder[j].ToString());
+                if (salary[i] != '9')
+                    onlyNine = false;
+            }
 
-                if (hightToLow && x > y)
-                {
-                    hightToLow = false;
-                }
+            if (hightToLow && onlyNine)
+            {
+                var x = Convert.ToInt32(salaryAsStringBuilder[0].ToString());
+                x++;
+                salaryAsStringBuilder[0] = x.ToString()[0];
+                salaryAsStringBuilder[salary.Length - 1] = salaryAsStringBuilder[0];
 
-                if (hightToLow && j - i == z)
+                for (int i = 0, j = salary.Length - 1; i < j; i++, j--)
                 {
-                    if (z == 2)
+                    if (j - i == z)
                     {
-                        var w = Convert.ToInt32(salaryAsStringBuilder[i++]);
-                        w++;
-                        salaryAsStringBuilder[i++] = w.ToString()[0];
+                        if (z == 2)
+                        {
+                            salaryAsStringBuilder[i += 1] = '0';
+                            continue;
+                        }
+                        else if (z == 1)
+                        {
+                            salaryAsStringBuilder[i] = '0';
+                            salaryAsStringBuilder[j] = '0';
+                            continue;
+                        }
                     }
-                    else if (z == 1)
+
+                    if (i == 0)
                     {
-                        x++;
-                        salaryAsStringBuilder[i] = x.ToString()[0];
-                        salaryAsStringBuilder[j] = x.ToString()[0];
+                        salaryAsStringBuilder[i += 1] = '0';
+                        salaryAsStringBuilder[j -= 1] = '0';
                         continue;
                     }
-
+                    salaryAsStringBuilder[i] = '0';
+                    salaryAsStringBuilder[j] = '0';
                 }
+            }
+            else
+            {
+                salaryAsStringBuilder[salary.Length - 1] = salary[0];
 
-                salaryAsStringBuilder[j] = salaryAsStringBuilder[i];
+                for (int i = 0, j = salary.Length - 1; i < j; i++, j--)
+                {
+                    var x = Convert.ToInt32(salaryAsStringBuilder[i].ToString());
+                    var y = Convert.ToInt32(salaryAsStringBuilder[j].ToString());
+
+                    if (hightToLow && x > y)
+                    {
+                        hightToLow = false;
+                    }
+
+                    if (hightToLow && j - i == z)
+                    {
+                        if (z == 2)
+                        {
+                            var w = Convert.ToInt32(salaryAsStringBuilder[i += 1].ToString());
+                            w++;
+                            salaryAsStringBuilder[i++] = w.ToString()[0];
+                        }
+                        else if (z == 1)
+                        {
+                            x++;
+                            salaryAsStringBuilder[i] = x.ToString()[0];
+                            salaryAsStringBuilder[j] = x.ToString()[0];
+                            continue;
+                        }
+
+                    }
+
+                    salaryAsStringBuilder[j] = salaryAsStringBuilder[i];
+                }
             }
 
             return salaryAsStringBuilder.ToString();
